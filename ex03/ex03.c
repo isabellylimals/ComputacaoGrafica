42cdb02e521647396a8fb32970655ce2ef4cc54c
Isabelly Lima <mslima5643@gmail.com>
2025-04-17 17:04:25 -0300

    up

---
+++ ex03/ex03.c
@@ -0,0 +1,27 @@
+//Deixar o fundo branco
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+FILE *fp = fopen("imagem.ppm", "w");
+
+
+int i, j;
+int linha = 100;
+int coluna = 100;
+fprintf(fp, "P3\n%d %d\n255\n", coluna, linha);
+for(i = 0; i < linha; i++){
+    for(j = 0; j < coluna; j++){
+        fprintf(fp, "%d %d %d ", 255, 255, 255);
+    }
+    fprintf(fp, "\n");
+}
+for(i = 0; i < linha; i++){
+    for(j = 0; j < coluna; j++){
+
+        if(i==)
+    }
+    fprintf(fp, "\n");
+}
+fclose(fp);
+return 0;}
---
+++ ex04/ex04.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    FILE *fp = fopen("imagem.ppm", "w");
+    int largura = 100, altura = 100;
+
+
+    fprintf(fp, "P3\n%d %d\n255\n", largura, altura);
+    int y, x;
+
+    
+    int centroX = largura / 2;
+    int centroY = altura / 2;
+
+    for (y = 0; y < altura; y++) {
+        for (x = 0; x < largura; x++) {
+            if (y == centroY - 10 && x >= centroX - 10 && x <= centroX + 10) {
+                fprintf(fp, "128 0 200 ");  
+            }
+
+            
+            else if (x == centroX && y >= centroY - 10 && y <= centroY + 15) {
+                fprintf(fp, "128 0 200 ");  
+            }
+
+           
+            else {
+                fprintf(fp, "0 0 0 ");
+            }
+        }
+        fprintf(fp, "\n");
+    }
+
+    fclose(fp);
+    return 0;
+}
