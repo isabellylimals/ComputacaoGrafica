c2ddc8546bbf110b1566b2dab7b8547d8aacf6fa
Isabelly Lima <mslima5643@gmail.com>
2025-04-27 17:56:04 -0300

    operações

---
+++ bibliotecadevetores/operacoes/op.c
@@ -0,0 +1,23 @@
+#include "op.h"
+void soma(int a[], int v[], int resultado[] ){
+    resultado[0]= a[0]+v[0];
+    resultado[1]= a[1]+v[1];
+
+}
+void subtracao(int a[], int v[], int resultado[]){
+    resultado[0]= a[0]-v[0];
+    resultado[1]= a[1]-v[1];
+}
+void soma2(int a[], int v[], int resultado[] ){
+    resultado[0]= a[0]+v[0];
+    resultado[1]= a[1]+v[1];
+    resultado[2]= a[2]+v[2];
+   
+}
+void subtracao2(int a[], int v[], int resultado[] ){
+    resultado[0]= a[0]-v[0];
+    resultado[1]= a[1]-v[1];
+    resultado[2]= a[2]-v[2];
+
+}
+
---
+++ bibliotecadevetores/operacoes/op.h
@@ -0,0 +1,7 @@
+#ifndef OP_H
+#define OP_H
+void soma(int a[], int v[], int resultado[] );
+void subtracao(int a[], int v[], int resultado[]);
+void soma2(int a[], int v[], int resultado[] );
+void subtracao2(int a[], int v[], int resultado[] );
+#endif
